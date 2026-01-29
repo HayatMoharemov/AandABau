387df5890bc8cfb886fcc9c7dc2aff8be1239a97
Hayat Moharemov <h.moharemov@gmail.com>
2026-01-29 23:21:06 +0100

    Added back end integrated admin functionality in the contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import ContactForm
+
+
+@admin.register(ContactForm)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'submitted_at']
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from career.validators import check_number_length, check_if_its_digits, phone_number_code_validator, name_validator
 
 class ContactForm(models.Model):
+    class Meta:
+        verbose_name = 'Contact inquiry'
+        verbose_name_plural = 'Contact inquiries'
 
     first_name = models.CharField(max_length=50,
                                   validators=[name_validator])
@@ -12,3 +15,4 @@ class ContactForm(models.Model):
                                          check_if_its_digits,
                                          phone_number_code_validator])
     description = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
