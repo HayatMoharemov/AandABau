1da38057d80fd50d01d9ec5d14d30b66c2afbffb
Hayat Moharemov <h.moharemov@gmail.com>
2026-02-02 21:50:25 +0100

    added background image inside the base.html and added the company logo in nav.html

--- AABau/urls.py
+++ AABau/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from AABau import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,9 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('feedback/', include('feedback.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
