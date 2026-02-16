308a1e2bfd5deb7cc06609ec5c24d16a86082d9d
Hayat Moharemov <h.moharemov@gmail.com>
2026-02-16 18:37:43 +0100

    Created models for the Services and OurTeam apps.

--- ourteam/models.py
+++ ourteam/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+from career.models import JobPositions
+
+class TeamMembersModel(models.Model):
+
+    class Meta:
+        verbose_name = 'Team Member'
+        verbose_name_plural = 'Team Members'
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    position = models.ForeignKey(JobPositions,
+                                 on_delete=models.PROTECT,
+                                 related_name='team_members')
+
+    def __str__(self):
+        return f"{self.name} - {self.position}"
--- services/models.py
+++ services/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from ourteam.models import TeamMembersModel
+
+class ServicesTypeModel(models.Model):
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
+    class ServiceTypeChoices(models.TextChoices):
+        FACADE_WORKS = 'Facade Works', 'Facade Works'
+        INSULATION = 'Insulation', 'Insulation'
+        PLASTERING = 'Plastering', 'Plastering'
+        RENOVATION = 'Renovation', 'Renovation'
+        ROOF_REPAIR = 'Roof Repair', 'Roof Repair'
+        SEWER_CONSTRUCTION = 'Sewer Construction', 'Sewer Construction'
+
+
+    type = models.CharField(max_length=50,
+                            choices=ServiceTypeChoices.choices)
+    short_description = models.TextField()
+    description = models.TextField()
+    overview_image = models.ImageField(upload_to='static/images/services')
+    detail_view_image = models.ImageField(upload_to='static/images/services')
+    responsible_person = models.ManyToManyField(TeamMembersModel,
+                                                related_name='services')
+
+    def __str__(self):
+        return f"{self.type}"
